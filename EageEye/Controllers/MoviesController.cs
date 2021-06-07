40234d8ee8c3369bddeda239f76683ba092066f1
Chukwudozie Okudo <okudopato@yahoo.com>
2021-06-07 06:04:42 +0100

    Add project files.

---
+++ EageEye/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using EageEye.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EageEye.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        //GET /movies/stats
+        [Route("/stats")]
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            string line;
+            List<Movie> movies = new List<Movie>();
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"c:\metadata.csv");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] words = line.Split(',');
+                movies.Add(new Movie(int.Parse(words[0]), words[1], words[2], words[3], int.Parse(words[4])));
+            }
+
+            file.Close();
+
+            return movies;
+        }
+
+
+        [HttpGet("{id}")]
+        //GET /metadata/:movieId
+        [Route("/metadata/:movieId")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // POST /metadata
+        [HttpPost]
+        [Route("/metadata")]
+        public ActionResult Create(Movie movie)
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ EageEye/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EageEye.Models
+{
+    public class Movie
+    {
+        private int v1;
+        private string v2;
+        private string v3;
+        private string v4;
+        private int v5;
+
+        public Movie(int v1, string v2, string v3, string v4, int v5)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+        public string Duration { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
+
